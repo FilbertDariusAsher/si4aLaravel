901bc7bfd15ed0179d61ea6ec78abdc3d704323c
Rimuru <filbertasherdarius@gmail.com>
2025-05-31 14:40:40 +0700

    update repo

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use App\Models\Prodi;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mahasiswa = mahasiswa::all();//perintah sql select * from maha
+        // dd($mahasiswa);//dump and die
+        return view('mahasiswa.index')->with('mahasiwa',$mahasiswa);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        $prodi = Prodi::all(); // ambil semua data prodi
+        return view('mahasiswa.create',compact('prodi'));// mengirimkan data prodi ke view mahasiswa.create
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->validate(
+        [
+            'npm' => 'required|unique:mahasiswa',
+            'nama' => 'required',
+            'jk' => 'required',
+            'tanggal_lahir' => 'required',
+            'tempat_lahir' => 'required',
+            'asal_sma' => 'required',
+            'prodi_id' => 'required',
+            'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($request->file('foto')) {
+            $file = $request->file('foto');// ambil file foto
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move(public_path('images'), $filename);// simpan file ke folder images
+            $input['foto'] = $filename;// simpan nama file ke database
+        }
+
+        Mahasiswa::create($input);
+        // redirect ke route mahasiswa.index
+        return redirect()->route('mahasiswa.index')->with('success', 'Mahasiswa Berhasil Ditambahkan');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.show', compact('mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($mahasiswa)
+    {
+        $mahasiswa=Mahasiswa::findOrFail($mahasiswa);
+        // dd($mahasiswa);
+
+        if ($mahasiswa->foto) {
+            $fotoPath = public_path('image/' .
+            $mahasiswa->foto);
+            if (file_exists($fotoPath)) {
+                unlink($fotoPath);
+            }
+        }
+
+        $mahasiswa->delete();
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Mahasiswa berhasil dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+use App\Http\Controllers\ProdiController;//jangan lupa import
+use App\Http\Controllers\FakultasController;//jangan lupa import
+use App\Http\Controllers\MahasiswaController;//jangan lupa import
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route::get('/profil',function (){
+    return view('profil');
+});
+
+route::resource('/fakultas',FakultasController::class);
+route::resource('/prodi',ProdiController::class);
+route::resource('/mahasiswa',MahasiswaController::class);
+Route::get('/dashboard', [DashboardController::class, 'index']);
